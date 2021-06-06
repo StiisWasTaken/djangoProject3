c7f3cb714ec9c98c04ffc6f71445c7422ad07f63
StiisWasTaken <karolniekurzak121@gmail.com>
2021-06-06 21:56:25 +0200

    Update without cart

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import (PostListView, PostDetailView,PostCreateView,PostUpdateView,PostDelateView,BikeDetailView,AccesorDetailView)
 from . import views
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),             #  blog\ bo '' jest puste a apka nazywa sie blog
     path('bikes/', views.bikes, name='blog-bikes'),     # blog\bike
+    path('bikes/<int:pk>/', BikeDetailView.as_view(), name='bikes-detail'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDelateView.as_view(), name='post-delete'),
     path('parts', views.parts, name='blog-parts'),
     path('accesories',views.accesories, name='blog-accesories'),
+    path('accesories/<int:pk>/',AccesorDetailView.as_view(), name='accesories-detail'),
     path('rent',views.rent, name='blog-rent'),
     #path('profile', views.profile, name='blog-profile'),
     path('cart', views.cart, name='blog-cart'),
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 from .models import Post
 from .models import Bike
 from .models import Accesor
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (ListView, DetailView, CreateView, UpdateView,DeleteView)
 
 # Create your views here.
 def home(request):
@@ -22,12 +23,48 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['titles','content']
+
+    def form_valid(self, form):
+        form.instance.author =self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post
+    fields = ['titles','content']
+
+    def form_valid(self, form):
+        form.instance.author =self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post=self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+class PostDelateView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = '/'
+    def test_func(self):
+        post=self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
 def bikes(request):
     context = {		'posts':Post.objects.all(),
                     'bikes':Bike.objects.all(),
                     'title':'Bikes'
 	}
     return render(request,'blog/bikes.html', context)
+
+class BikeDetailView(DetailView):
+    model = Bike
 
 def parts(request):
     context = {
@@ -41,6 +78,10 @@ def accesories(request):
                        'title': 'Accesories'
                        }
     return render(request,'blog/accesories.html',context)
+
+class AccesorDetailView(DetailView):
+    model = Accesor
+
 
 def rent(request):
     context ={          'title': 'Rent'
